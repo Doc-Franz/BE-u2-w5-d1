d773653e27707c9ab27ed6f065b3bfa9f9f8c78c
Doc-Franz <federico.franzi94@gmail.com>
2025-02-03 16:57:02 +0100

    first commit

---
+++ demoSpring/src/main/java/com/epicode/demo/Menu.java
@@ -0,0 +1,16 @@
+package com.epicode.demo;
+
+public class Menu {
+
+    protected String name;
+    protected int calories;
+    protected double price;
+
+    public Menu(String name, int calories, double price){
+        this.name = name;
+        this.calories = calories;
+        this.price = price;
+    }
+
+
+}
